25fe7713b772b0221e86aabb08dab4d6c9360fca
seshizak <seshizak@gmail.com>
2025-07-12 10:03:20 +0900

    Update filtering_and_ordering.sql

--- filtering_and_ordering.sql
+++ filtering_and_ordering.sql
@@ -1,3 +1,7 @@
+-- Goal: Find campaigns that started in June 2025 with a high budget.
+-- I’m filtering based on the date range for June and only including campaigns with a budget over 5000.
+-- I'm also ordering the results from highest to lowest budget to prioritize bigger campaigns.
+
 SELECT campaign_id, campaign_name
 FROM Campaigns
 WHERE start_date >= '2025-06-01' AND start_date <= '2025-06-30'
